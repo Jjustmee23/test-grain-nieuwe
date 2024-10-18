7110b6915f7fa8fe6b062308b4a903d68e128357
Abdur Rehman Wasti <mwasti.bee18seecs@seecs.edu.pk>
2024-10-18 19:20:01 +0500

    Model Updated

--- mill/models.py
+++ mill/models.py
@@ -1,6 +1,22 @@
 from django.db import models
-from django.utils import timezone
-from datetime import timedelta
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, blank=False)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Factory(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True, blank=True, related_name='factories')
+    status = models.BooleanField(default=False)
+    error = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f" {self.name} at {self.created_at}"
 
 class Device(models.Model):
     name = models.CharField(max_length=255)
@@ -8,53 +24,40 @@ class Device(models.Model):
     selected_counter = models.CharField(max_length=50, default='counter_1')
     status = models.BooleanField(default=False)
     factory = models.ForeignKey('Factory', on_delete=models.SET_NULL, null=True, blank=True, related_name='devices')
+    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name or self.serial_number
 
+# Devicedata are transections of each device wich will be added daily and not updated or deleted
 class DeviceData(models.Model):
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    counter_1 = models.IntegerField(default=0)
-    counter_2 = models.IntegerField(default=0)
-    counter_3 = models.IntegerField(default=0)
-    counter_4 = models.IntegerField(default=0)
+    device = models.ForeignKey(Device, null=True, on_delete=models.SET_NULL,db_index=True)
     daily_total = models.IntegerField(default=0)
     weekly_total = models.IntegerField(default=0)
     monthly_total = models.IntegerField(default=0)
     yearly_total = models.IntegerField(default=0)
-    created_at = models.DateTimeField(default=timezone.now)
+    reference = models.CharField(max_length=255)    
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Data for {self.device.name} at {self.created_at}"
 
-class City(models.Model):
-    name = models.CharField(max_length=100, unique=True, null=False)
-    status = models.BooleanField(default=True)
+# CounterData values will be created every 30 minutes by MQTT. They will come from another DB 
+class CountersData(models.Model):
+    device = models.ForeignKey(Device,null=True, on_delete = models.SET_NULL,db_index=True)
+    counter_1 = models.IntegerField(default=0)
+    counter_2 = models.IntegerField(default=0)
+    counter_3 = models.IntegerField(default=0)
+    counter_4 = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-
-class Factory(models.Model):
-    name = models.CharField(max_length=255)
-    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True, blank=True)
-    status = models.BooleanField(default=False)
-    dailyTotal = models.IntegerField(default=0)
-    error = models.BooleanField(default=False)
-    last_update = models.DateTimeField(default=timezone.now)
+        return f"Counters Data for {self.device.name} at {self.created_at}"
     
-    def check_status(self):
-        if self.last_update and timezone.now() - self.last_update > timedelta(minutes=5):
-            self.error = True
-            self.status = False
-        else:
-            self.error = False
-            self.status = True
-        self.save()
-
 class MQTTData(models.Model):
     device_id = models.CharField(max_length=255)
     counter = models.IntegerField()
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         managed = False
