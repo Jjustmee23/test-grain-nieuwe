18ad4653a3ae153ff1a7823da4728545e355ea95
Abdur Rehman Wasti <mwasti.bee18seecs@seecs.edu.pk>
2025-01-05 00:28:35 +0500

    Added Postress db in prod

---
+++ config/settings_prod.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME','testdb'),
+        'USER': os.getenv('DB_USER','testuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD','testpassword'),
+        'HOST': os.getenv('DB_HOST', '45.154.238.114'),
+        'PORT': os.getenv('DB_PORT', '5433'),
+    }
+}
