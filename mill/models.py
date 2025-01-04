68fe3cccfda4b3eac0d493ec4a5e6d14e6c97002
Abdur Rehman Wasti <mwasti.bee18seecs@seecs.edu.pk>
2025-01-05 01:45:02 +0500

    Updated models.py

--- mill/models.py
+++ mill/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+# City model
 class City(models.Model):
     name = models.CharField(max_length=100, unique=True, blank=False)
     status = models.BooleanField(default=True)
@@ -8,58 +10,56 @@ class City(models.Model):
     def __str__(self):
         return self.name
 
+# Factory model
 class Factory(models.Model):
     name = models.CharField(max_length=255)
-    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True, blank=True, related_name='factories')
-    status = models.BooleanField(default=False)
+    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True, blank=True, related_name='factories')
+    status = models.BooleanField(default=True)
     error = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f" {self.name} at {self.created_at}"
 
+    def __str__(self):
+        return self.name
+
+# Device model
 class Device(models.Model):
+    id = models.IntegerField(primary_key=True,unique=True)
     name = models.CharField(max_length=255)
-    serial_number = models.CharField(max_length=255, unique=True)
     selected_counter = models.CharField(max_length=50, default='counter_1')
     status = models.BooleanField(default=False)
-    factory = models.ForeignKey('Factory', on_delete=models.SET_NULL, null=True, blank=True, related_name='devices')
-    created_at = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.name or self.serial_number
-
-# Devicedata are transections of each device wich will be added daily and not updated or deleted
-class DeviceData(models.Model):
-    device = models.ForeignKey(Device, null=True, on_delete=models.SET_NULL,db_index=True)
-    daily_total = models.IntegerField(default=0)
-    weekly_total = models.IntegerField(default=0)
-    monthly_total = models.IntegerField(default=0)
-    yearly_total = models.IntegerField(default=0)
-    reference = models.CharField(max_length=255)    
+    factory = models.ForeignKey(Factory, on_delete=models.SET_NULL, null=True, blank=True, related_name='devices')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Data for {self.device.name} at {self.created_at}"
+        return self.name
 
-# CounterData values will be created every 30 minutes by MQTT. They will come from another DB 
-class CountersData(models.Model):
-    device = models.ForeignKey(Device,null=True, on_delete = models.SET_NULL,db_index=True)
-    counter_1 = models.IntegerField(default=0)
-    counter_2 = models.IntegerField(default=0)
-    counter_3 = models.IntegerField(default=0)
-    counter_4 = models.IntegerField(default=0)
+# ProductionData model to update hourly, daily, weekly, monthly, and yearly production
+class ProductionData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='production_data')
+    daily_production = models.IntegerField(default=0)
+    weekly_production = models.IntegerField(default=0)
+    monthly_production = models.IntegerField(default=0)
+    yearly_production = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"Counters Data for {self.device.name} at {self.created_at}"
-    
-class MQTTData(models.Model):
-    device_id = models.CharField(max_length=255)
-    counter = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
+    updated_at = models.DateTimeField(auto_now=True)
     class Meta:
-        managed = False
-        db_table = 'mqtt_data'
-        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['device', 'created_at']),
+        ]
+    def __str__(self):
+        return f"Production Data for {self.device.name} at {self.updated_at}"
+
+# TransactionData model to transactioning hourly, daily, weekly, monthly, and yearly production
+class TransactionData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='transaction_data')
+    daily_production = models.IntegerField(default=0)
+    weekly_production = models.IntegerField(default=0)
+    monthly_production = models.IntegerField(default=0)
+    yearly_production = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['device', 'created_at']),
+        ]
+    def __str__(self):
+        return f"Production Data for {self.device.name} at {self.created_at}"
